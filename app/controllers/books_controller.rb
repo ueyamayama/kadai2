afd20aa040b3529b4d368195c449e9d9ba52b626
EC2 Default User <ec2-user@ip-172-31-39-84.ec2.internal>
2022-06-02 13:39:57 +0000

    [Finish]課題2アプリケーション作成(基礎)

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,48 @@
+class BooksController < ApplicationController
+  def create
+    flash[:notice] = "Book was successfully created."
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    flash[:notice] = "Book was successfully updated."
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    flash[:notice] = "Book was successfully destroyed."
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end 
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root to: 'homes#top'
+  #post 'books' => 'books#create'
+  #get 'books' => 'books#index'
+  #get 'books/:id' => 'books#show' , as: 'book'
+  #get 'books/:id/edit' => 'books#edit' , as: 'edit_book'
+  #patch 'books/:id' => 'books#update' , as: 'update_book'
+  #delete 'books/:id' => 'books#destroy' , as: 'destroy_book'
+  resources :books
+end
